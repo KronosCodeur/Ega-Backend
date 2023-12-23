1148e4e07d85d1d2740904ad47cb87a7876ee2a3
KronosCodeur <piraliarris@gmail.com>
2023-12-23 17:05:26 +0000

    bd config and Client entity creation

--- src/main/java/tg/iai/ega/entities/Client.java
+++ src/main/java/tg/iai/ega/entities/Client.java
@@ -1,2 +1,24 @@
-package tg.iai.ega.entities;public class Client {
+package tg.iai.ega.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String firstName;
+    String lastName;
+    String phone;
+    String email;
+    String address;
 }

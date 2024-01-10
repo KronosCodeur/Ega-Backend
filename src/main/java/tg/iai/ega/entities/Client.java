81b14408be8bbe4fd790f258d126dbf7f6a822e3
Kronos_Codeur <nayodahe@gmail.com>
2024-01-10 21:38:17 +0000

    client entity updated

--- src/main/java/tg/iai/ega/entities/Client.java
+++ src/main/java/tg/iai/ega/entities/Client.java
@@ -2,28 +2,65 @@ package tg.iai.ega.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import tg.iai.ega.enums.Gender;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Client {
+@Builder
+public class Client implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
-    String firstName;
-    String lastName;
-    String phone;
-    String email;
-    Date birthday;
-    String address;
-    Gender gender;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private LocalDate birthday;
+    private String address;
+    private String nationality;
+    private Gender gender;
+    private String password;
     @OneToMany(mappedBy = "client",cascade = CascadeType.ALL)
     List<Account> accounts;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }

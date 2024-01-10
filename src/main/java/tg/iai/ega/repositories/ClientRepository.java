dc45c7541f85cfb8da12fcd6ab21c7846329c9e4
Kronos_Codeur <bedinadejosue39@gmail.com>
2024-01-10 21:40:02 +0000

    findClientByEmail method declaration

--- src/main/java/tg/iai/ega/repositories/ClientRepository.java
+++ src/main/java/tg/iai/ega/repositories/ClientRepository.java
@@ -3,8 +3,10 @@ package tg.iai.ega.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tg.iai.ega.entities.Client;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client,Long> {
 
+
+    Optional<Client> findClientByEmail(String email);
 }

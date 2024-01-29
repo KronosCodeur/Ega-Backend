543805f437254ec322ee8502c4c1bef24c335295
KronosCodeur <piraliarris@gmail.com>
2024-01-29 20:01:04 +0000

    IAccountService updated

--- src/main/java/tg/iai/ega/services/IAccountService.java
+++ src/main/java/tg/iai/ega/services/IAccountService.java
@@ -1,13 +1,14 @@
 package tg.iai.ega.services;
 
 
+import tg.iai.ega.dto.AccountDto;
 import tg.iai.ega.entities.Account;
 
 import java.util.List;
 
 public interface IAccountService {
     List<Account> showAllAccounts();
-    Account saveAccount(Account account);
+    Account saveAccount(AccountDto account);
     Account getOneAccount(Long id);
     Account getAccountByNumber(String number);
     void deleteAccount(Long id);

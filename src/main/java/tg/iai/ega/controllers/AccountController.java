c09d5759babbfe769e5b672ae5f12b9174cf0c4b
KronosCodeur <piraliarris@gmail.com>
2024-01-29 19:59:35 +0000

    methods naming changed

--- src/main/java/tg/iai/ega/controllers/AccountController.java
+++ src/main/java/tg/iai/ega/controllers/AccountController.java
@@ -3,9 +3,12 @@ package tg.iai.ega.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tg.iai.ega.dto.AccountDto;
+import tg.iai.ega.dto.AccountOutputDto;
 import tg.iai.ega.entities.Account;
 import tg.iai.ega.services.AccountService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,48 +18,48 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
     @GetMapping("/accounts")
-    public List<Account> showAllAccounts(){
-        return accountService.showAllAccounts();
+    public List<AccountOutputDto> showAllAccounts(){
+        var accounts = accountService.showAllAccounts();
+        List<AccountOutputDto> accountOutputDtoList = new ArrayList<>();
+        for (var account : accounts) {
+            accountOutputDtoList.add(new AccountOutputDto().entityToDto(account));
+        }
+        return  accountOutputDtoList;
     }
     @PostMapping("/accounts")
-    public Account getOneAccount(@RequestBody Account account){
+    public Account saveAccount(@RequestBody AccountDto account){
         return accountService.saveAccount(account);
     }
     @GetMapping("/accounts/{id}")
-    public Account getOneAccount(@PathVariable Long id){
-        return accountService.getOneAccount(id);
-    }
-    @PutMapping("/accounts/{id}")
-    public Account updateClient(@RequestBody Account account, @PathVariable Long id){
-        account.setId(id);
-        return accountService.saveAccount(account);
+    public AccountOutputDto getOneAccount(@PathVariable Long id){
+        return new AccountOutputDto().entityToDto(accountService.getOneAccount(id));
     }
     @DeleteMapping("/accounts/{id}")
     public void deleteAccount(@PathVariable Long id){
         accountService.deleteAccount(id);
     }
-    @GetMapping("/accounts/{number}")
-    public Account getAccountByNumber(@PathVariable String number){
-        return accountService.getAccountByNumber(number);
+    @PostMapping("/accounts/getAccountByNumber")
+    public AccountOutputDto getAccountByNumber(@RequestParam String number){
+        return new AccountOutputDto().entityToDto(accountService.getAccountByNumber(number));
     }
     @PostMapping("/deposit/{number}")
-    public ResponseEntity<String> effectuerDepot(@PathVariable String number, @RequestParam float montant) {
-        accountService.effectuerDepot(number, montant);
+    public ResponseEntity<String> makeDeposit(@PathVariable String number, @RequestParam float amount) {
+        accountService.makeDeposit(number, amount);
         return ResponseEntity.ok("Dépôt effectué avec succès.");
     }
 
     @PostMapping("/withdrawal/{number}")
-    public ResponseEntity<String> effectuerRetrait(@PathVariable String number, @RequestParam float montant) {
-        accountService.effectuerRetrait(number, montant);
+    public ResponseEntity<String> makeWithdrawal(@PathVariable String number, @RequestParam float amount) {
+        accountService.makeWithdrawal(number, amount);
         return ResponseEntity.ok("Retrait effectué avec succès.");
     }
 
     @PostMapping("/transfer")
-    public ResponseEntity<String> effectuerVirement(
+    public ResponseEntity<String> makeTransfer(
             @RequestParam String sourceNumber,
             @RequestParam String destinationNumber,
-            @RequestParam float montant) {
-        accountService.effectuerVirement(sourceNumber, destinationNumber, montant);
+            @RequestParam float amount) {
+        accountService.makeTransfer(sourceNumber, destinationNumber, amount);
         return ResponseEntity.ok("Virement effectué avec succès.");
     }
 }
